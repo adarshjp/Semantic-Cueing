bafa79e2137fd22680f724a203c3ba72719205a1
adarshjp <adarsh.24padmashali@gmail.com>
2021-11-03 19:26:48 +0530

    added add Q routes

--- models/question.js
+++ models/question.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const questionSchema = new mongoose.Schema({
   question: {
     data: Buffer,
-    required: true,
     contentType: String,
   },
   answer: {
@@ -16,54 +15,23 @@ const questionSchema = new mongoose.Schema({
     max: 5,
     default: 1,
   },
-  hint1:{
-    img:{
-      data: Buffer,
-      contentType: String,
-    },
-    score:{
-      type:Number,
-      required:true,
-      min:10,
+  hints:[
+    {
+      hint:{
+        data: Buffer,
+        contentType: String,
+      },
+      score:{
+        type:Number,
+        required:true,
+        min:10,
+      }
     }
-  },
-  hint2:{
-    img:{
-      data: Buffer,
-      contentType: String,
-    },
-    score:{
-      type:Number,
-      required:true,
-      min:10,
-    }
-  },
-  hint3:{
-    img:{
-      data: Buffer,
-      contentType: String,
-    },
-    score:{
-      type:Number,
-      required:true,
-      min:10,
-    }
-  },
-  hint4:{
-    img:{
-      data: Buffer,
-      contentType: String,
-    },
-    score:{
-      type:Number,
-      required:true,
-      min:10,
-    }
-  },
+  ],
   score: {
     type: Number,
     required: true,
     min: 100,
   },
 });
-exports.Question = mongoose.model("Question", questionSchema);
+module.exports = mongoose.model("Question", questionSchema);
