fd1705564ee6027e5ceb94867deb2f1ed75bd5b3
adarshjp <adarsh.24padmashali@gmail.com>
2021-11-12 15:59:56 +0530

    create test post req

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const {get_home_doctor,get_create_test} = require("../controllers/doctor");
+const {get_home_doctor,get_create_test,post_create_test} = require("../controllers/doctor");
 const {isLoggedIn,isDoctor}=require('../controllers/Auth/middlewares')
 router.get("/create/test", isLoggedIn, isDoctor, get_create_test);
 router.get("/home/doctor", isLoggedIn, isDoctor, get_home_doctor);
+router.post("/create/test", isLoggedIn, isDoctor, post_create_test);
 module.exports = router;
