74d3b6b31cc454e7d36a3c0b6ad71ed594889a64
adarshjp <adarsh.24padmashali@gmail.com>
2021-12-14 17:19:14 +0530

    added some routes

--- routes/patient.js
+++ routes/patient.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { isLoggedIn,isPatient,isTestAssigned}=require("../controllers/Auth/middlewares")
+const { isLoggedIn,isPatient,isTestAssigned,isTestCompleted}=require("../controllers/Auth/middlewares")
 const {get_home_patient,get_start_test,post_start_test}=require("../controllers/patient")
 router.get("/home/patient/:id",isLoggedIn, isPatient, get_home_patient);
 router.get("/start/test/:id",isLoggedIn, isPatient,isTestAssigned,get_start_test)
