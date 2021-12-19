6207b9fdaa47738e84a9dff3550e284de3ee8d50
Java simplilearn <blenjoswinsaldanha@gmail.com>
2021-12-19 11:15:23 +0530

    added homepages

--- index.js
+++ index.js
@@ -60,3 +60,8 @@ app.use(patientRoutes)
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started at port 3000");
 });
+
+app.get("/test", (req,res) => {
+    res.render("doctor")
+})
+
