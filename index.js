228a135ef0ddbea5d07639b43896ac820d399533
adarshjp <adarsh.24padmashali@gmail.com>
2022-01-07 22:41:23 +0530

    refactoring sidebar

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const methodOverride= require("method-override");
 app.use(express.static("public"));
+app.use(express.static("public/images"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 app.set("view engine","ejs")
+
 const connectionParams = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -29,22 +31,23 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 app.use(cookieParser("keyboard cat"));
 app.use(flash());
+
 const User = require("./models/user");
 app.use(session({
   secret: process.env.secret,
   resave: true,
   saveUninitialized: true
 }));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 app.use(function (req, res, next) {
   res.locals.currentUser = req.user;
   res.locals.message = req.flash();
   next();
 });
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 const adminRoutes = require("./routes/admin");
 const generalRoutes = require("./routes/general");
