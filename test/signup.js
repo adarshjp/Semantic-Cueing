9ec760f1d2eb5c5a9dca2809f1195d55aaf1d732
adarshjp <adarsh.24padmashali@gmail.com>
2022-05-29 10:52:42 +0530

    test commit to main

--- test/signup.js
+++ test/signup.js
@@ -14,6 +14,38 @@ exports.signupTest = () => {
                 });
         });
         it('should render register page and return 200', (done) => {
+            /* 
+                /register is a protected route, so we need to login first 
+                before we can access it.
+                and only admin can access it
+            */
+            let user=request.agent(app);
+            user
+                .post('/login')
+                .send({
+                    username: process.env.ADMIN_USERNAME,
+                    password: process.env.ADMIN_PASSWORD
+                })
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .expect('Location', '/home/admin')
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    user
+                        .get('/register')
+                        .expect(200)
+                        .end((err, res) => {
+                            if (err) return done(err);
+                            done();
+                        });
+                });
+        }).timeout(100000);
+        it('should render register page and return 200', (done) => {
+            /* 
+                /register is a protected route, so we need to login first 
+                before we can access it.
+                and only admin can access it
+            */
             let user=request.agent(app);
             user
                 .post('/login')
@@ -36,26 +68,60 @@ exports.signupTest = () => {
                 });
         }).timeout(100000);
     });
-    // describe('POST /signup ', () => {
-    //     it('correct password and username', (done) => {
-    //         request(app)
-    //             .post('/signup')
-    //             .send({
-    //                 username: 'admin',
-    //                 password: 'admin',
-    //                 displaypic: '',
-    //                 email: '',
-    //             })
-    //             .set('Accept', 'application/json')
-    //             .set('Content-Type', 'application/json')
-    //             .expect('Location', '/login')
-    //             .end(function (err, res) {
-    //                 expect(res.status).to.equal(302)
-    //                 // res.header['Location'].should.include('/home')
-    //                 // res.text.to.equal('Login successful')
-    //                 if (err) return done(err);
-    //                 return done();
-    //             });
-    //     }).timeout(100000)
-    // });
+    describe('POST /register ', () => {
+        it('should redirect to /login and return 302', (done) => {
+            request(app)
+                .post('/register')
+                .send({
+                    username: 'admin',
+                    password: 'admin',
+                    email:'adarsh.24padmashali@gmail.com',
+                    role:'admin',
+                })
+                .set('Accept', 'application/json')
+                .set('Content-Type', 'application/json')
+                .expect('Location', '/login')
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    done();
+                });
+        }).timeout(100000);
+        // it('', (done) => {
+        //     let user=request.agent(app);
+        //     user
+        //         .post('/login')
+        //         .send({
+        //             username: process.env.ADMIN_USERNAME,
+        //             password: process.env.ADMIN_PASSWORD
+        //         })
+        //         .set('Accept', 'application/json')
+        //         .set('Content-Type', 'application/json')
+        //         .expect('Location', '/home/admin')
+        //         .end(function (err, res) {
+        //             if (err) return done(err);
+        //             user
+        //                 .post('/register')
+        //                 .send({
+        //                     username: 'admin',
+        //                     password: 'admin',
+        //                     role:'admin',
+        //                     email:'adarsh@gmail.com',
+        //                     name: 'admin',
+        //                     age:24,
+        //                     displaypic: '',
+
+        //                 })
+        //                 .set('Content-Type', 'application/json')
+        //                 .expect(200)
+        //                 .end((err, res) => {
+        //                     if (err) return done(err);
+        //                     done();
+        //                 });
+                
+        //             });
+        //             //done();
+        //         //});
+        // }).timeout(100000);
+    });
+
 };
